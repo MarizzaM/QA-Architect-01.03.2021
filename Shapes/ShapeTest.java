e05545522afabcf4ad49c7b02368028b517f69ba
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-03-02 21:25:02 +0200

    Update ShapeTest.java

--- Shapes/ShapeTest.java
+++ Shapes/ShapeTest.java
@@ -8,21 +8,21 @@ public class ShapeTest  {
 
     @Test
     public void test_get_area_of_a_circle(){
-        Circle circle = new Circle("kolobok", 2);
+        Circle circle = new Circle("kolobok", test_get_area_of_a_circle_radius);
 
         assertEquals(Math.PI * Math.pow(TestData.test_get_area_of_a_circle_radius, 2), circle.getArea());
     }
 
     @Test
     public void test_get_perimeter_of_a_circle(){
-        Circle circle = new Circle("small picture", 1000);
+        Circle circle = new Circle("small picture", test_get_perimeter_of_a_circle_radius);
 
         assertEquals(2 * Math.PI * TestData.test_get_perimeter_of_a_circle_radius, circle.getPerimeter());
     }
 
     @Test
     public void test_get_perimeter_of_a_rectangle(){
-        Rectangle rectangle = new Rectangle("big picture", 20,88);
+        Rectangle rectangle = new Rectangle("big picture", test_get_perimeter_of_a_rectangle_a, TestData.test_get_perimeter_of_a_rectangle_b);
 
         assertEquals(2 * (TestData.test_get_perimeter_of_a_rectangle_a + TestData.test_get_perimeter_of_a_rectangle_b),
                 rectangle.getPerimeter());
@@ -30,7 +30,7 @@ public class ShapeTest  {
 
     @Test
     public void test_get_area_of_a_rectangle(){
-        Rectangle rectangle = new Rectangle("small picture", 4,8);
+        Rectangle rectangle = new Rectangle("small picture", test_get_area_of_a_rectangle_a, test_get_area_of_a_rectangle_b);
 
         assertEquals(TestData.test_get_area_of_a_rectangle_a * TestData.test_get_area_of_a_rectangle_b,
                 rectangle.getArea());
@@ -38,7 +38,7 @@ public class ShapeTest  {
 
     @Test
     public void test_get_perimeter_of_a_square() {
-        Square square = new Square("SpongeBob", 2.2);
+        Square square = new Square("SpongeBob", test_get_perimeter_of_a_square_a);
 
         assertEquals(4 * TestData.test_get_perimeter_of_a_square_a,
                 square.getPerimeter());
@@ -46,7 +46,7 @@ public class ShapeTest  {
 
     @Test
     public void test_get_area_of_a_square(){
-        Square square = new Square("SquarePants", 4.4);
+        Square square = new Square("SquarePants", test_get_area_of_a_square_a);
 
         assertEquals(Math.pow(TestData.test_get_area_of_a_square_a,2),
                 square.getArea());
